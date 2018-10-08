fd33f86195e22a37a78669ca100676bc0444d9d2
marleythepuli <nix818@gmail.com>
2018-10-08 11:32:27 -0700

    remove comment

--- controller.rb
+++ controller.rb
@@ -9,7 +9,7 @@ class ApplySuggestionController < GitContentControllers
     files = { path_string => contents }
 
     if commit_blob_change_to_repo_for_user(current_repository, current_user, branch, ref.target_oid, files, commit_message)
-      head :ok #send me an EMAIL!!!
+      head :ok
     else
       head :unprocessable
     end
