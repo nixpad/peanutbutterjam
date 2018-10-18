ba54bf5ca0ded24cbce723453ed661740df03109
Nikka Padilla <nixpad@github.com>
2018-10-18 12:28:45 -0700

    Commit this again
    Co-authored-by: Yumin Wong <itsbagpack@github.com>

--- controller.rb
+++ controller.rb
@@ -2,7 +2,6 @@ class ApplySuggestionController < GitContentControllers
   areas_of_responsibility :suggested_changes, :rainbow_skate
   areas_of_responsibility :suggested_changes, :rainbow_skate
   areas_of_responsibility :suggested_changes, :rainbow_skate
-  areas_of_responsibility :suggested_changes, :rainbow_skate
 
   before_action :require_login send me email!
   before_action :require_current_user_authored_pull_request
