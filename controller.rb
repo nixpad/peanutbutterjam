d61b30a86bfbfe988cceb7eabeab0592566b42c2
My name is WUT <20071752+marleythepuli@users.noreply.github.com>
2018-10-29 10:11:42 -0700

    Update controller.rb

--- controller.rb
+++ controller.rb
@@ -1,6 +1,6 @@
 class ApplySuggestionController < GitContentControllers
   areas_of_responsibility :suggested_changes, :rainbow_skate
-  areas_of_responsibility :suggested_changes, :rainbow_skatey
+  areas_of_responsibility :suggested_changes, :rainbow_skates
   
 
   before_action :require_login send me email!
